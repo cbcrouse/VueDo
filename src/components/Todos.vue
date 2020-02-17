b166f5daf6bf3e6b7260617e1b32e50bbce7b6d9
Casey <crouse.casey@gmail.com>
2020-02-16 21:51:43 -0700

    refactor(Todos): Consolidated components
    Moved the contents of Todo.vue into Todos.vue and created a new TodoModel class for a simpler instantiation of the todo data array.

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,52 +1,58 @@
 <template>
-  <v-card class="mx-auto" max-width="400" tile>
-    <v-list-item v-for="todo in todos" :key="todo.id">
-      <v-list-item-content>
-        <v-list-item-title>{{ todo.title }}</v-list-item-title>
-        <v-list-item-subtitle>{{ todo.id }}: {{ todo.completed }}</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-  </v-card>
+  <v-container fill-height align-center>
+    <v-list subheader two-line flat>
+      <v-list-item-group multiple v-for="todo in todos" :key="todo.id">
+        <v-list-item>
+          <template v-slot:default="{ active, toggle }">
+            <v-list-item-action>
+              <v-checkbox
+                v-model=todo.completed
+                color="primary"
+                @click="toggle"
+              ></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ todo.title }}</v-list-item-title>
+              <v-list-item-subtitle>{{ todo.id }}: {{ todo.completed }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-container>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
 import { Log } from "@/decorators/log";
 
+export class TodoModel {
+  id: String | undefined;
+  title: String | undefined;
+  completed: Boolean | undefined;
+
+  constructor(id: string, title: string, completed: boolean) {
+    this.id = id;
+    this.title = title;
+    this.completed = completed;
+  }
+}
+
 @Component
 export default class Todos extends Vue {
   // Data property
-  todos: object[] = [];
+  todos: TodoModel[] = [];
 
   // Initializing todo list using Lifecycle hooks.
   @Log
   created() {
     this.todos = [
-      {
-        id: "1",
-        title: "todo 1",
-        completed: false
-      },
-      {
-        id: "2",
-        title: "todo 2",
-        completed: false
-      },
-      {
-        id: "3",
-        title: "todo 3",
-        completed: false
-      },
-      {
-        id: "4",
-        title: "todo 4",
-        completed: false
-      },
-      {
-        id: "5",
-        title: "todo 5",
-        completed: false
-      }
+      new TodoModel("1", "todo 1", false),
+      new TodoModel("2", "todo 2", false),
+      new TodoModel("3", "todo 3", false),
+      new TodoModel("4", "todo 4", false),
+      new TodoModel("5", "todo 5", false)
     ];
   }
 }
